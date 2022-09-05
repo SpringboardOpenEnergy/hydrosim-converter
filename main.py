016f9cc539766c16bdcc5098b5bc0066149a3c45
Steinar <s.r.eriksen@gmail.com>
2022-09-05 11:22:07 +0200

    Updated

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import os, codecs
 
 
 def process_input(file, parser):
-    parser.parse_xml(file)
-    G, all_nodes, all_edges=generate_network(parser)
-    add_color(G, all_nodes, all_edges)
-    pyplot_graph(G, './pyplot_' + parser.parser_name() + '.png')
+    parser.parse_xml(file)  #Parses the file and populates plants, wrtr, rsrv lists with names of nodes
+    G, all_nodes, all_edges=generate_network(parser)  #Generates a networkx graph based on the parsed lists
+    add_color(G, all_nodes, all_edges)  #Adds color codes
+    pyplot_graph(G, './pyplot_' + parser.parser_name() + '.png')  #Exports to PNG file
     fig=plot_graph(G)
-    fname='./d3_' + parser.parser_name() + '.html'
+    fname='./d3_' + parser.parser_name() + '.html'   #Exports to D3
     html=fig.to_html()
     file = codecs.open(fname, "w", "utf-8")
     file.write(str(html))
@@ -23,7 +23,9 @@ def process_input(file, parser):
     rdf_gen(G, all_nodes, all_edges, './rdf_' + parser.parser_name() + '.txt')
 
 if __name__ == '__main__':
+    # 2 different parsers. One includes all watercourses as given in the XML - some of which duplicates links
+    # the other combines information to reduce number of nodes
     parser=SimModelParser()
-    process_input("./Begna.basic.xml", parser)
+    process_input("./hydsimexport.xml", parser)
     parser=SimModelParser2()
-    process_input("./Begna.basic.xml", parser)
+    process_input("./hydsimexport.xml", parser)
